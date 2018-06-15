16ad29233272156c4eeaab84cbb742c2b4852579
Beyonce Knowles <beyonce@thinkful.com>
2018-06-14 20:15:15 -0700

    second add

--- server.js
+++ server.js
@@ -52,6 +52,22 @@ app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
 })
 
+
+app.post('/recipes', jsonParser, (req, res)=> {
+    // create a variable that is requred
+    const needKeyRecipe = ["name", "ingredients"];
+    for (let i = 0; i<needKeyRecipe.length; i++){
+        const keyRecipe = needKeyRecipe[i];
+        if(!(keyRecipe in req.body)){
+            console.log("input key and values dude");
+            return res.status(400)
+        }
+    }
+    const listing = Recipes.create(req.body.name, req.body.ingredients);
+    res.status(201).json(listing);
+})
+
+
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
